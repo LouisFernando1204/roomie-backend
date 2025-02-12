ba5d4d01cb207b0276e32503edbd195fbd2eedfa
Louis Fernando <ferlouis55@gmail.com>
2025-02-12 11:48:35 +0700

    Add frontend domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigin = [
-    'https://www.yoursite.com',
+    'https://roomie-dapp.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:5173'
